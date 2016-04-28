237bdddf110cc44e3fa4eec03c22025f838f7a39
Ivan Juresa <ivanjuresa611@gmail.com>
2016-04-28 11:01:23 +0200

    PCF8575 Library

---
+++ Sensors/PCF8575/pcf8575.c
@@ -0,0 +1,61 @@
+
+/**
+  * Copyright (C) Ivan Juresa, 2016
+	*
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+	*
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	*
+**/
+
+/* Includes */
+#include "pcf8575.h"
+
+/* Read and write address */
+uint8_t array[2];
+
+void PCF8575_Init(uint8_t readAddress, uint8_t writeAddress) {
+	array[0] = readAddress;
+	array[1] = writeAddress;
+}
+
+void PCF8575_Clear(I2C_HandleTypeDef *I2CxHandle) {
+	uint8_t output[2];
+	output[0] = 0x00;
+	output[1] = 0x00;
+	
+	HAL_I2C_Master_Transmit(I2CxHandle, (uint16_t)array[1], output, 2, 1000);
+}
+
+void PCF8575_Set(I2C_HandleTypeDef *I2CxHandle) {
+	uint8_t output[2];
+	output[0] = 0xFF;
+	output[1] = 0xFF;
+	
+	HAL_I2C_Master_Transmit(I2CxHandle, (uint16_t)array[1], output, 2, 1000);
+}
+
+int PCF8575_Read(I2C_HandleTypeDef *I2CxHandle) {
+	uint8_t input[2];
+	
+	HAL_I2C_Master_Receive(I2CxHandle, (uint16_t)array[0], input, 2, 1000);	
+	
+	return ((input[1] << 8) | input[0]);
+}
+
+void PCF8575_Write(I2C_HandleTypeDef *I2CxHandle, int data) {
+	uint8_t output[2];
+	output[0] = data;
+	output[1] = data >> 8;
+	
+	HAL_I2C_Master_Transmit(I2CxHandle, (uint16_t)array[1], output, 2, 1000);
+}
---
+++ Sensors/PCF8575/pcf8575.h
@@ -0,0 +1,65 @@
+
+/**
+  * Copyright (C) Ivan Juresa, 2016
+	*
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+	*
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	*
+**/
+ 
+ #ifndef __PCF8575_H__
+ #define __PCF8575_H__
+ 
+ #include "stm32f4xx_hal.h"
+ 
+ /**
+ * @brief  	Initializes PCF8575 write and read address
+ * @param  	readAddress: PCF8575 read address
+ * @param		writeAddress: PCF8575 write address
+ * @retval	None
+ **/
+ void PCF8575_Init(uint8_t readAddress, uint8_t writeAddress);
+ 
+ /**
+ * @brief  	All outputs goes low
+ * @note		All pins are set as OUTPUT
+ * @param  	*I2CxHandle: Pointer to I2C which is used
+ * @retval	None
+ **/
+ void PCF8575_Clear(I2C_HandleTypeDef *I2CxHandle);
+
+ /**
+ * @brief  	All outputs goes high
+ * @note		All pins are set as INPUTS		
+ * @param  	*I2CxHandle: Pointer to I2C which is used
+ * @retval	None
+ **/
+ void PCF8575_Set(I2C_HandleTypeDef *I2CxHandle);
+
+ /**
+ * @brief  	Reads pins
+ * @param  	*I2CxHandle: Pointer to I2C which is used
+ * @retval	16bit data
+ **/
+ int PCF8575_Read(I2C_HandleTypeDef *I2CxHandle);
+ 
+  /**
+ * @brief  	Setting pins as input or output
+ * @param  	*I2CxHandle: Pointer to I2C which is used
+ * @param  	data: value of pins 
+ * @retval	None
+ **/
+ void PCF8575_Write(I2C_HandleTypeDef *I2CxHandle, int data);
+ 
+ #endif   // __PCF8575_H__
+ 
